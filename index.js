bf9a192842d6af08f633ac3bbd1a3b5b51070d88
Amio <amio.cn@gmail.com>
2017-03-31 16:38:27 +0800

    Add intro.html.

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 const qs = require('querystring')
 const Url = require('url')
 const micro = require('micro')
-const snarkdown = require('snarkdown')
 const Nightmare = require('nightmare')
-
-const helpHTML = snarkdown(require('fs').readFileSync('./README.md', 'utf-8'))
 
 const screenshot = async (req, res) => {
   const query = qs.parse(Url.parse(req.url).query)
   const { url, height = 640, width = 960 } = query
 
-  if (!url) return helpHTML
+  if (!url) return require('fs').readFileSync('./intro.html', 'utf-8')
 
-  return Nightmare()
+  return Nightmare({gotoTimeout: 6000})
     .viewport(parseInt(width), parseInt(height))
     .goto(url)
     .screenshot()
     .end(result => {
       res.setHeader('Content-Type', 'image/png')
       return result
-    }, error => {
-      console.error(error)
-      return error
     })
 }
 
-micro(screenshot).listen(3000)
+micro(screenshot).listen(3001)
