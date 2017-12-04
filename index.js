ff18665259396e628729a88d15cf2d875fa82138
Amio <amio.cn@gmail.com>
2017-12-04 17:13:27 +0800

    Replace CDP with puppeteer

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
-const chromeDaemon = require('./lib/chrome-daemon.js')
 const launchServer = require('./lib/server.js')
-const debuglog = require('./lib/debuglog.js')
 
-const CHROME_PORT = 9222
 const SERVER_PORT = 3000
 
-chromeDaemon.launch({ port: CHROME_PORT })
-  .then(chrome => {
-    launchServer({ port: SERVER_PORT, chromePort: CHROME_PORT })
-  })
-  .catch(e => {
-    debuglog('ERR_CRITICAL', e)
-  })
+launchServer({ port: SERVER_PORT })
 
 process.on('uncaughtException', err => {
-  debuglog(`UncaughtException: ${err}`)
+  console.error(`UncaughtException: ${err}`)
   process.exit(1)
 })
